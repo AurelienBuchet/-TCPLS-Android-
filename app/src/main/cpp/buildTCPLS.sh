4ddefdc039dca6eaa87339c507e155c6ae88ea13
aubuchet <aurelien.buchet@student.uclouvain.be>
2022-06-03 09:57:25 +0200

    update path

--- app/src/main/cpp/buildTCPLS.sh
+++ app/src/main/cpp/buildTCPLS.sh
@@ -7,4 +7,4 @@ make clean
 cmake . -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=${target} -DANDROID_NATIVE_API_LEVEL=28 -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_STATIC_LIB="On" -DBUILD_SHARED_LIBS="On" -DOPENSSL_INCLUDE_DIR="../openssl/include"
 make
 
-cp libpicotcpls-jni.so liblog.so libpicotls-core.so libpicotls-minicrypto.so libpicotls-openssl.so /home/aubuchet/Documents/memoire/TCPLS/app/app/libs/$target
+cp libpicotcpls-jni.so liblog.so libpicotls-core.so libpicotls-minicrypto.so libpicotls-openssl.so /home/aubuchet/Documents/tcpls_app/app/app/libs/$target
